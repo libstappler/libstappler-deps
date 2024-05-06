5fe124209df84939eb23420faec1955fac26eb32
SBKarr <sbkarr@stappler.org>
2024-05-06 08:21:32 +0800

    WAMR library update

--- linux/wamr.mk
+++ linux/wamr.mk
@@ -25,8 +25,7 @@ LIBNAME = wasm-micro-runtime
 CFLAGS := -I$(PREFIX)/include $(ARCH_CFLAGS)
 LDFLAGS := $(CRT_LIB) -L$(PREFIX)/lib
 
-CONFIGURE := \
-	-DCMAKE_BUILD_TYPE=Release \
+CONFIGURE_ARGS := \
 	-DCMAKE_C_COMPILER_TARGET="$(TARGET)" \
 	-DCMAKE_C_FLAGS_INIT="$(CFLAGS)" \
 	-DCMAKE_EXE_LINKER_FLAGS_INIT="$(LDFLAGS)" \
@@ -36,7 +35,6 @@ CONFIGURE := \
 	-DBUILD_SHARED_LIBS=OFF \
 	-DWAMR_BUILD_PLATFORM=linux \
 	-DWAMR_BUILD_INTERP=1 \
-	-DWAMR_BUILD_FAST_INTERP=1 \
 	-DWAMR_BUILD_AOT=1 \
 	-DWAMR_BUILD_JIT=0 \
 	-DWAMR_BUILD_LIBC_BUILTIN=1 \
@@ -46,12 +44,48 @@ CONFIGURE := \
 	-DWAMR_BUILD_MODULE_INST_CONTEXT=1 \
 	-DWAMR_BUILD_MULTI_MODULE=1 \
 	-DWAMR_BUILD_BULK_MEMORY=1 \
+	-DWAMR_BUILD_SIMD=0 \
+	-DWAMR_BUILD_LIB_PTHREAD=1 \
+	-DWAMR_BUILD_LIB_PTHREAD_SEMAPHORE=1 \
+	-DWAMR_BUILD_LIB_WASI_THREADS=1
 
-all:
+CONFIGURE := \
+	-DCMAKE_BUILD_TYPE=Release \
+	$(CONFIGURE_ARGS) \
+	-DWAMR_BUILD_FAST_INTERP=1
+
+CONFIGURE_DEBUG := \
+	-DCMAKE_BUILD_TYPE=Debug \
+	$(CONFIGURE_ARGS) \
+	-DWAMR_DISABLE_WRITE_GS_BASE=1 \
+	-DWAMR_BUILD_DEBUG_INTERP=1 \
+	-DWAMR_BUILD_DEBUG_AOT=1 \
+	-DWAMR_BUILD_FAST_INTERP=0
+
+INCLUDES := $(PREFIX)/include/wamr/wasm_export.h $(PREFIX)/include/wamr/lib_export.h
+
+$(PREFIX)/include/wamr/wasm_export.h:
+	mkdir -p $(PREFIX)/include/wamr
+	cp -f $(LIB_SRC_DIR)/$(LIBNAME)/core/iwasm/include/wasm_export.h $(PREFIX)/include/wamr/wasm_export.h
+
+$(PREFIX)/include/wamr/lib_export.h:
+	mkdir -p $(PREFIX)/include/wamr
+	cp -f $(LIB_SRC_DIR)/$(LIBNAME)/core/iwasm/include/lib_export.h $(PREFIX)/include/wamr/lib_export.h
+
+$(PREFIX)/lib/libvmlib-release.a:
 	@mkdir -p $(LIBNAME)
 	cd $(LIBNAME); cmake $(CONFIGURE) $(LIB_SRC_DIR)/$(LIBNAME)
-	cd $(LIBNAME); cmake  --build . --config Release --target install
-	rm $(PREFIX)/lib/libiwasm.so
+	cd $(LIBNAME); cmake  --build . --config Release
+	cp -f $(LIBNAME)/libvmlib.a $(PREFIX)/lib/libvmlib-release.a
 	rm -rf $(LIBNAME)
+
+$(PREFIX)/lib/libvmlib-debug.a:
+	@mkdir -p $(LIBNAME)
+	cd $(LIBNAME); cmake $(CONFIGURE_DEBUG) $(LIB_SRC_DIR)/$(LIBNAME)
+	cd $(LIBNAME); cmake  --build . --config Debug
+	cp -f $(LIBNAME)/libvmlib.a $(PREFIX)/lib/libvmlib-debug.a
+	rm -rf $(LIBNAME)
+
+all: $(INCLUDES) $(PREFIX)/lib/libvmlib-release.a $(PREFIX)/lib/libvmlib-debug.a
 
 .PHONY: all
